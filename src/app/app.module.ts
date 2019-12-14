2512438fe007b4bd58f6aa1de043f517bddc41f1
aaronshivers <aaron@moonmail.org>
2019-12-14 06:10:27 -0600

    feature/training - add past-training component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { SignupComponent } from './auth/signup/signup.component';
 import { TrainingComponent } from './training/training/training.component';
 import { CurrentTrainingComponent } from './training/current-training/current-training.component';
 import { NewTrainingComponent } from './training/new-training/new-training.component';
+import { PastTrainingComponent } from './training/past-training/past-training.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { NewTrainingComponent } from './training/new-training/new-training.compo
     SignupComponent,
     TrainingComponent,
     CurrentTrainingComponent,
-    NewTrainingComponent
+    NewTrainingComponent,
+    PastTrainingComponent
   ],
   imports: [
     BrowserModule,
