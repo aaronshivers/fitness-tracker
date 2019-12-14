271675943c5177626ecff42930ffff2f2f8b4064
aaronshivers <aaron@moonmail.org>
2019-12-14 06:18:33 -0600

    feature/welcome - enable welcome component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { SignupComponent } from './auth/signup/signup.component';
 import { LoginComponent } from './auth/login/login.component';
 import { TrainingComponent } from './training/training/training.component';
 import { RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 const Routes = [
-  // { path: '', component: WelcomeComponent },
+  { path: '', component: WelcomeComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
   { path: 'training', component: TrainingComponent },
