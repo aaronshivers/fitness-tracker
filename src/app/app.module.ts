1ea62befecea798867f6aa20302f7ae9ca3cb065
aaronshivers <aaron@moonmail.org>
2019-12-14 06:08:57 -0600

    feature/training - add training component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { TrainingComponent } from './training/training/training.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    TrainingComponent
   ],
   imports: [
     BrowserModule,
