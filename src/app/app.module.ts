1830a41af043870609597643a054418844bb6b2c
aaronshivers <aaron@moonmail.org>
2019-12-14 06:00:17 -0600

    refactor/material.component - move material module to its own directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
