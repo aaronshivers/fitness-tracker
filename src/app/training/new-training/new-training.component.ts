455e6e9b0faf004668cd4aecb987290e69452461
aaronshivers <aaron@moonmail.org>
2019-12-14 22:38:13 -0600

    feature/training - add button to switch to current-training component

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 interface Exercise {
   value: string;
@@ -11,6 +11,7 @@ interface Exercise {
   styleUrls: [ './new-training.component.css' ],
 })
 export class NewTrainingComponent implements OnInit {
+  @Output() startTraining = new EventEmitter<boolean>();
   exercises: Exercise[] = [
     { value: 'push-ups', viewValue: 'Push Ups' },
     { value: 'pull-ups', viewValue: 'Pull Ups' },
@@ -23,4 +24,7 @@ export class NewTrainingComponent implements OnInit {
   ngOnInit() {
   }
 
+  onStartTraining() {
+    this.startTraining.emit();
+  }
 }
