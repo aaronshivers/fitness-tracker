cb7e7abcf3a4d64cc01c0835be7d901726ded276
aaronshivers <aaron@moonmail.org>
2019-12-16 06:51:37 -0600

    feature/firebase - add env variables for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDhmKq7KiaTcKLPrNsIFAHOjWWggEJENTA',
+    authDomain: 'fitness-tracker-1a404.firebaseapp.com',
+    databaseURL: 'https://fitness-tracker-1a404.firebaseio.com',
+    projectId: 'fitness-tracker-1a404',
+    storageBucket: 'fitness-tracker-1a404.appspot.com',
+    messagingSenderId: '351775625615',
+  }
 };
